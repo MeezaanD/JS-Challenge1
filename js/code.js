84ae167eec85bc4b69bcf427440a35b5bc0585ff
Meezaan Davids <meezaandavids365@gamil.com>
2022-11-08 11:09:36 +0200

    update

---
+++ js/code.js
@@ -0,0 +1,27 @@
+// Document
+
+// let userName = 'name'
+// let userSurname = 'surname'
+// let numb1 = 15000
+// let numb2 = 500
+// let salary = eval(`${numb1}+${numb2}`);
+
+// // document.write(userName);
+// // document.write("<br>"+ userSurname);
+// document.write(`<br>Name:${userName}<br>Surname:${userSurname}`)
+// document.write("<br>"+ salary);
+
+// Console
+
+let userName = prompt('Enter your name')
+let userSurname = prompt('Enter your Surname')
+let userSalary = prompt('Please enter your salary')
+// let numb1 = 15000
+// let numb2 = 500
+let salarySum = eval (userSalary) + 500;
+
+console.log(`Name:${userName}`);
+console.log(`Surname:${userSurname}`);
+console.log(`Salary:${salarySum}`);
+
+
